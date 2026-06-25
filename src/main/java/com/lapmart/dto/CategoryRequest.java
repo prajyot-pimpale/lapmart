0ad71ecbe3f8837b23cbd1813c33a24b50dc4a83
Prajyot Pimpale <pimpaleprajyot@gmail.com>
2026-06-25 22:46:50 +0530

    CategoryRequest dto added.

---
+++ src/main/java/com/lapmart/dto/CategoryRequest.java
@@ -0,0 +1,22 @@
+package com.lapmart.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class CategoryRequest {
+
+    @NotBlank(message = "Category name is required")
+    private String name;
+
+	public CategoryRequest(@NotBlank(message = "Category name is required") String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
+}
